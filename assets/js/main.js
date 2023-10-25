e94e8a332ad683002f6cb70a123952798fdd7c71
WINDOWS-10\Administrator <laiphuhieu123@gmail.com>
2023-10-25 17:28:57 +0700

    Section 8

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,27 @@ function initSlider() {
     loop: false,
     speed: 400,
     nav: false,
-    controls: true,
     prevButton: ".btn-left",
     nextButton: ".btn-right",
   });
 }
 
+function initSlider2() {
+  var slider = tns({
+    container: ".section-clients-wrapper",
+    items: 1,
+    controls: true,
+    loop: false,
+    speed: 400,
+    nav: true,
+    controlsText: [
+      '<span><img style="width: 24px" src="./assets/img/arrow.svg" alt="" /></span>',
+      '<span><img style="width: 24px" src="./assets/img/arrow2.svg" alt="" /></span>',
+    ],
+  });
+}
+
 window.onload = (event) => {
   initSlider();
+  initSlider2();
 };
